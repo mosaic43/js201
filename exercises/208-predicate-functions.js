3055ca0c0ace7f7109d108175e1dcb9d6be10ddc
Chantee Smith <chantee.smith@gmail.com>
2019-06-28 22:23:21 -0500

    Update to exercise 208, fixed question 2 and finished question 3.

--- exercises/208-predicate-functions.js
+++ exercises/208-predicate-functions.js
@@ -45,15 +45,27 @@ function isVowel(character){
 // isOdd('7') --> false
 // isOdd(3.14) --> false
 
+///Is variable a number or string
+///Is variable even or odd
+
+
 function isEven (aNumber) {
     var even = false
-    if (aNumber % 2 == 0 ){even = true}
+
+    if (Number.isInteger(aNumber)) {
+        if (aNumber % 2 === 0 ){even = true}
+    }
+  
     return even
 }
 
+
 function isOdd (anotherNumber) {
     var odd = false
-    if (anotherNumber % 2 > 0 ) {odd = true}
+
+    if (Number.isInteger(anotherNumber)){
+        if (anotherNumber % 2 !== 0 ) {odd = true}
+    }
     return odd
 }
 
@@ -76,8 +88,68 @@ function isOdd (anotherNumber) {
 // isCapitalCity('Strawberry', 'Mango') --> false
 
 
-function isCapitalCity(usState, city) {
-var states []
 
+
+
+function isCapitalCity(usState, city) {
+
+    var StatesAndCapitals =
+    {
+        "Alabama"         : "Montgomery",
+        "Alaska"          : "Juneau",
+        "Arizona"         : "Phoenix",
+        "Arkansas"        : "Little Rock",
+        "California"      : "Sacramento",
+        "Colorado"        : "Denver",
+        "Connecticut"     : "Hartford",
+        "Delaware"        : "Dover",
+        "Florida"         : "Tallahassee",
+        "Georgia"         : "Atlanta",
+        "Hawaii"          : "Honolulu",
+        "Idaho"           : "Boise",
+        "Illinois"        : "Springfield",
+        "Indiana"         : "Indianapolis",
+        "Iowa"            : "Des Moines",
+        "Kansas"          : "Topeka",
+        "Kentucky"        : "Frankfort",
+        "Louisiana"       : "Baton Rouge",
+        "Maine"           : "Augusta",
+        "Maryland"        : "Annapolis",
+        "Massachusetts"   : "Boston",
+        "Michigan"        : "Lansing",
+        "Minnesota"       : "Saint Paul",
+        "Mississippi"     : "Jackson",
+        "Missouri"        : "Jefferson City",
+        "Montana"         : "Helena",
+        "Nebraska"        : "Lincoln",
+        "Nevada"          : "Carson City",
+        "New Hampshire"   : "Concord",
+        "New Jersey"      : "Trenton",
+        "New Mexico"      : "Santa Fe",
+        "New York"        : "Albany",
+        "North Carolina"  : "Raleigh",
+        "Ohio"            : "Columbus",
+        "Oklahoma"        : "Oklahoma City",
+        "Oregon"          : "Salem",
+        "Pennsylvania"    : "Harrisburg",
+        "Rhode Island"    : "Providence",
+        "South Carolina"  : "Columbia",
+        "North Dakota"    : "Bismarck",
+        "South Dakota"    : "Pierre",
+        "Tennessee"       : "Nashville",
+        "Texas"           : "Austin",
+        "Utah"            : "Salt Lake City",
+        "Tennessee"       : "Nashville",
+        "Vermont"         : "Montpelier",
+        "Virginia"        : "Richmond",
+        "Washington"      : "Olympia",
+        "West Virginia"   : "Charleston",
+        "Wisconsin"       : "Madison" 
+    };
+
+    var isCorrectCapital = false
+
+if (StatesAndCapitals[usState]=== city)  {isCorrectCapital = true}   
+return isCorrectCapital
 
 }
